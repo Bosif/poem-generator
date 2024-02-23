f7433203e8029e5a1b6afa0359cd4064965e5126
Bosif <boannarutjes@gmail.com>
2024-02-23 01:22:30 +0100

    Added final javascript

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     "You are a poem expert and love to write short poems. Your mission is to create a 4-line poem and separate each line with a <br />. Make sure to follow the user instructions and do not change any styling. Do not add a title and finish the poem signed with 'SheCodes AI' inside a <strong> element";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = "Generating...";
+
   console.log("Generating your personal poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
