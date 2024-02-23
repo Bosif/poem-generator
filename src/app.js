284da4527f188521e1586faa74dcf209ac8a0898
Bosif <boannarutjes@gmail.com>
2024-02-23 01:02:01 +0100

    updated Javascript to work with form input

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemContainer = document.querySelector("poem");
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Roses are red...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "c8a5e25887f10t1o4fc6a4fb7b8c3bcf";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a poem expert and love to write short poems. Your mission is to create a 4-line poem and separate each line with a <br />. Make sure to follow the user instructions and do not change any styling. Do not add a title and finish the poem signed with 'SheCodes AI' inside a <strong> element";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating your personal poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
